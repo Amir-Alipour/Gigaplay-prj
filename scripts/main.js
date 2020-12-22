6158c5fbdb65abb6010666a5c4c8752a537a7295
Amir Alipour <amir.alipour12127@gmail.com>
2020-12-22 11:11:12 +0330

    added navigation

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+$(function () {
+
+    var nav_menu_counter = 0;
+    $('.nav-main').click(function () {
+        if (nav_menu_counter === 0) {
+            nav_menu_counter = 1;
+            $(this).find('i').removeClass('fa-ellipsis-h');
+            $(this).find('i').addClass('fa-times');
+            $('.nav-main-items').css('display', 'block');
+            $('.nav-main-items').css('opacity', '1');
+        } else {
+            nav_menu_counter = 0;
+            $(this).find('i').addClass('fa-ellipsis-h');
+            $(this).find('i').removeClass('fa-times');
+            $('.nav-main-items').css('opacity', '0');
+            $('.nav-main-items').css('display', 'none');
+        }
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
