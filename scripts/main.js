8f334b6520dab391baaeaf195ad6aa5a4e3a0cee
Amir Alipour <amir.alipour12127@gmail.com>
2020-12-22 21:49:03 +0330

    link the <a> tags

--- scripts/main.js
+++ scripts/main.js
@@ -84,7 +84,13 @@ $(function () {
 
     // ----------------------------------
 
-
+    $('.bottom-circle i').click(function () {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        });
+    });
 
 
 
