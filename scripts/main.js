a122884826deaadbe960a393256cbc9f59ebce39
Amir Alipour <amir.alipour12127@gmail.com>
2020-12-22 15:45:26 +0330

    header complete

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 $(function () {
+    AOS.init();
 
     var nav_menu_counter = 0;
     $('.nav-main').click(function () {
@@ -17,16 +18,60 @@ $(function () {
         }
     })
 
+    $(window).on('scroll', function () {
+        console.log($(window).scrollTop());
+    })
+    1815
 
-    $(window).on('scroll',function () {
+    $(window).on('scroll', function () {
         var scroll = $(window).scrollTop();
-        if (scroll > 1523){
-            $('.main-bg').css('position','absolute');
-            $('.main-bg').css('bottom','0');
+
+        if(scroll > 925){
+            $('#card-featued').css('position','fixed');
+            $('#card-featued').css('top','100px');
+            $('#card-featued').css('right','109px');
+            $('#card-featued').css('width','488px');
+            $('#footer-of-header').css('margin-top', '500px');
+        }else{
+            $('#card-featued').removeAttr('style');
+            $('#card-featued').css('position','relative');
+            $('#card-featued').css('top','900px');
+            $('#footer-of-header').css('margin-top', '0');
+        }
+
+1781
+        if (scroll > 1700) {
+            $('.main-bg').css('position', 'absolute');
+            $('.main-bg').css('top', '1700px');
+            $('.main-bg').css('background-repeat', 'repeat');
+        } else {
+            $('.main-bg').css('position', 'fixed');
+            $('.main-bg').css('top', '0');
+            $('.main-bg').css('background-repeat', 'no-repeat');
+        }
+
+        if (scroll > 1780) {
+            $('.befor-ellip').css('opacity', '0');
+            setTimeout(() => {
+                $('.befor-ellip').css('position', 'absolute');
+            }, 500);
+        } else {
+            $('.befor-ellip').css('opacity', '1');
+            setTimeout(() => {
+                $('.befor-ellip').css('position', 'fixed');
+            }, 500);
+        }
+
+        if(scroll > 1781){
+            $('#card-featued').removeAttr('style');
+            $('#card-featued').css('position','absolute');
+            $('#card-featued').css('top','1880px');
+            $('#card-featued').css('width','488px');
+            $('#card-featued').css('right','109px');
+            $('#footer-of-header').css('margin-top', '500px');
         }
     })
 
-    1523
 
 
 
